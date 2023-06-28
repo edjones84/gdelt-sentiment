f9706d990b22a3b2a9cf341b99db3a34056dfc05
edjones84 <ejones84@icloud.com>
2023-06-28 13:01:18 +0100

    working front end

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-from fastapi import FastAPI
+import json
+
+from fastapi import FastAPI, Query
 import uvicorn
+from fastapi.encoders import jsonable_encoder
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import Response
 
 from gdelt_api import make_request, extract_titles, sentiment_dictionary_gen
 from schema import QueryResponses
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8000"
+    "http://localhost:8080",
+    "http://192.168.1.203:8080"  # Add the origin that's making the request
+]
 
-@app.post("/query")
-def query_endpoint(query_request: str) -> QueryResponses:
-    response_json = make_request(query_request)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/", response_model=QueryResponses)
+def query_endpoint(queryInput: str = Query(..., alias="queryInput")) -> Response:
+    response_json = make_request(queryInput)
     titles = extract_titles(response_json)
     scores_out = sentiment_dictionary_gen(titles)
-    return scores_out
+    return jsonable_encoder(json.loads(scores_out.json()))
 
 
 if __name__ == '__main__':
